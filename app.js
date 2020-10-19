51ddb22bece5728fbb66f2c7f7035240fd2dd90e
Adam Whitman <adam.whitmanpt@gmail.com>
2020-10-19 13:21:57 -0700

    Mostly done, one error showing up and haven't done class component

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // TODO
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList groceries={['Beer', 'Pizza']}/>
+  </div>
+);
+
+var GroceryListLItem = (props) => (
+  <li>{props.el}</li>
+)
+
+function GroceryList(props) {
+  return (
+    <ul>
+      {props.groceries.map((el)=> (
+        <GroceryListLItem el = {el}/>
+      ))}
+    </ul>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
